a0b3ea394357f72193e797dd93abe2dce7109b08
Tuti Bueno <tuti.bueno@gmail.com>
2026-07-11 15:07:08 -0300

    pagina de manutenção ajustes

--- wp-content/themes/versao-ltda-theme/templates/maintenance.php
+++ wp-content/themes/versao-ltda-theme/templates/maintenance.php
@@ -6,25 +6,8 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
-$settings = vltda_maintenance_get_settings();
-
-$logo_url = vltda_maintenance_resolve_image_url(
-	(int) ($settings['logo_attachment'] ?? 0),
-	'logo'
-);
-
-$bg_url = vltda_maintenance_resolve_image_url(
-	(int) ($settings['background_attachment'] ?? 0),
-	'background'
-);
-
-$title = (string) ($settings['title'] ?? '');
-$text = (string) ($settings['text'] ?? '');
-$show_animation = (bool) ($settings['show_animation'] ?? true);
-$show_counter = (bool) ($settings['show_counter'] ?? true);
-$launch_date = (string) ($settings['launch_date'] ?? '');
-$button_text = (string) ($settings['button_text'] ?? '');
-$button_url = (string) ($settings['button_url'] ?? '');
+$logo_src = vltda_asset('images/logo_versao_ltda_svg.svg');
+$bg_src = vltda_asset('images/maintenance/bg.jpg');
 
 ?>
 <!doctype html>
@@ -34,46 +17,22 @@ $button_url = (string) ($settings['button_url'] ?? '');
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php wp_head(); ?>
 
-		<link rel="preload" as="image" href="<?php echo esc_url($bg_url); ?>">
+		<link rel="preload" as="image" href="<?php echo esc_url($bg_src); ?>">
 	</head>
 
-	<body <?php body_class('vltda-maintenance'); ?> data-bg="<?php echo esc_attr($bg_url); ?>">
+	<body <?php body_class('vltda-maintenance'); ?> data-bg="<?php echo esc_attr($bg_src); ?>">
 		<?php wp_body_open(); ?>
 
-		<div class="vltda-maintenance" role="main">
+		<main class="vltda-maintenance" role="main">
 			<div class="vltda-maintenance__bg" aria-hidden="true"></div>
-			<div class="vltda-maintenance__inner">
-				<div class="vltda-maintenance__brand">
-					<img class="vltda-maintenance__logo" src="<?php echo esc_url($logo_url); ?>" alt="" />
-				</div>
-
-				<?php if ($title !== '') : ?>
-					<h1 class="vltda-maintenance__title"><?php echo esc_html($title); ?></h1>
-				<?php endif; ?>
-
-				<?php if ($text !== '') : ?>
-					<div class="vltda-maintenance__text"><?php echo wp_kses_post($text); ?></div>
-				<?php endif; ?>
-
-				<?php if ($show_animation) : ?>
-					<div class="vltda-maintenance__animation" aria-hidden="true">
-						<div class="vltda-maintenance__spinner"></div>
-					</div>
-				<?php endif; ?>
-
-				<?php if ($show_counter && $launch_date !== '') : ?>
-					<div class="vltda-maintenance__counter" data-launch-date="<?php echo esc_attr($launch_date); ?>" aria-label="Contador regressivo"></div>
-				<?php endif; ?>
-
-				<?php if ($button_text !== '' && $button_url !== '') : ?>
-					<div class="vltda-maintenance__cta">
-						<a class="vltda-maintenance__button" href="<?php echo esc_url($button_url); ?>"><?php echo esc_html($button_text); ?></a>
-					</div>
-				<?php endif; ?>
+			<div class="vltda-maintenance__center">
+				<img class="vltda-maintenance__logo" src="<?php echo esc_url($logo_src); ?>" alt="" />
+				<div class="vltda-maintenance__subtitle">Porque alguns jogos merecem mais</div>
 			</div>
-		</div>
+		</main>
 
 		<?php wp_footer(); ?>
 	</body>
 </html>
+
 
